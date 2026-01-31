2bf73edf53ed6b9737ae5aa9edc85d284b69e03a
Jesika Kotaeva <DKotaeva@inno.tech>
2026-01-31 16:18:53 +0300

    реализован парсер файлов с циклом while

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
-        System.out.println("Сумма: " + (firstNumber+secondNumber));
-        System.out.println("Разность: " + (firstNumber-secondNumber));
-        System.out.println("Произведение: " + (firstNumber*secondNumber));
-        System.out.println("Частное: " + ((double)firstNumber/secondNumber));
+        Scanner scanner = new Scanner(System.in);
+        int validFileCount = 0;
+
+        while (true) {
+            System.out.print("Введите путь к файлу: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            boolean exists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!exists) {
+                System.out.println("Файл не существует.");
+                continue;
+            }
+
+            if (isDirectory) {
+                System.out.println("Указанный путь ведет к папке, а не к файлу.");
+                continue;
+            }
+
+            validFileCount++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + validFileCount);
+        }
     }
 }
