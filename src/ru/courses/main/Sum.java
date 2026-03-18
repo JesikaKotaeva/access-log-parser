b0a869b42412a8e0d1b762239ff385f61e572d91
Jesika Kotaeva <DKotaeva@inno.tech>
2026-03-18 16:30:20 +0300

    Добавлен класс Sum с обработкой NumberFormatException

---
+++ src/ru/courses/main/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    public static void main(String[] args) {
+        double sum = 0;
+
+        for (String arg : args) {
+            try {
+                sum += Double.parseDouble(arg);
+            } catch (NumberFormatException e) {}
+        }
+
+        System.out.println("result: "+sum);
+    }
+}
